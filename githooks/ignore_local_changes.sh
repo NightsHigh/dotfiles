87d1f70f37fee8ac1b309fd26997d07a7f8ade6b
eske4 <eske.larsen@gmail.com>
2025-03-13 19:52:17 +0100

    Test

--- githooks/ignore_local_changes.sh
+++ githooks/ignore_local_changes.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-# Remove files from the Git index (using -r for directories)
-git rm --cached links/.config/hypr/monitors.conf
-git rm --cached links/.config/BetterDiscord/themes/pywal-discord-default.theme.css
-git rm --cached links/.config/wpg/sequences
-git rm --cached links/.config/BetterDiscord/
-git rm --cached links/.config/gtk-3.0/settings.ini
-git rm --cached links/.config/hypr/conf/colors-hyprland.conf
-git rm --cached links/.config/swaync/colors-swaync.css
-git rm --cached links/.config/tofi/config
-git rm --cached links/.config/waybar/colors-waybar.css
-git rm --cached links/.config/waypaper/config.ini
-git rm --cached links/.config/wlogout/colors-wlogout.css
-git rm --cached links/.config/wpg/
-git rm --cached links/.config/xsettingsd/xsettingsd.conf
-git rm --cached links/.gtkrc-2.0
-git rm --cached links/.icons/default/ -r
-git rm --cached links/Wallpapers/ -r
+# Ensure the files are removed from staging before commit
+git reset HEAD links/.config/hypr/monitors.conf
+git reset HEAD links/.config/BetterDiscord/themes/pywal-discord-default.theme.css
+git reset HEAD links/.config/wpg/sequences
+git reset HEAD links/.config/BetterDiscord/
+git reset HEAD links/.config/gtk-3.0/settings.ini
+git reset HEAD links/.config/hypr/conf/colors-hyprland.conf
+git reset HEAD links/.config/swaync/colors-swaync.css
+git reset HEAD links/.config/tofi/config
+git reset HEAD links/.config/waybar/colors-waybar.css
+git reset HEAD links/.config/waypaper/config.ini
+git reset HEAD links/.config/wlogout/colors-wlogout.css
+git reset HEAD links/.config/wpg/
+git reset HEAD links/.config/xsettingsd/xsettingsd.conf
+git reset HEAD links/.gtkrc-2.0
+git reset HEAD links/.icons/default/
+git reset HEAD links/Wallpapers/
 
-echo "Avoiding the upload of desktop-specific settings"
+# Optionally, you can add these to .gitignore if not already done
+# echo "links/.config/hypr/monitors.conf" >> .gitignore
+# echo "links/Wallpapers/" >> .gitignore
+
+echo "Excluded desktop-specific settings from the commit."
